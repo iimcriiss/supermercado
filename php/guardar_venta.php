6dde7e7d906564ebdb0ea7bd19bb8e2226645445
yoiner <albreyacosta14@gmail.com>
2026-03-23 17:49:40 -0500

    Edicion de SEGURIDAD Y BUENAS PRÁCTICAS

--- php/guardar_venta.php
+++ php/guardar_venta.php
@@ -1,10 +1,13 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "tienda");
+require_once 'db.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-$total = 0;
+if (!$data) {
+    die("No hay datos de venta");
+}
 
+$total = 0;
 foreach ($data as $producto) {
     $total += $producto['precio'] * $producto['cantidad'];
 }
@@ -18,9 +21,16 @@ $id_venta = $conn->insert_id;
 $stmt_detalle = $conn->prepare("INSERT INTO ventas_detalle (id_venta, producto, cantidad, precio) VALUES (?, ?, ?, ?)");
 
 foreach ($data as $producto) {
-    $stmt_detalle->bind_param("isid", $id_venta, $producto['nombre'], $producto['cantidad'], $producto['precio']);
+    $p_nombre = filter_var($producto['nombre'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $p_cantidad = (int)$producto['cantidad'];
+    $p_precio = (float)$producto['precio'];
+    $stmt_detalle->bind_param("isid", $id_venta, $p_nombre, $p_cantidad, $p_precio);
     $stmt_detalle->execute();
 }
 
 echo "Venta guardada correctamente";
+
+$stmt->close();
+$stmt_detalle->close();
+$conn->close();
 ?>
