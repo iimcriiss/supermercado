7b232c5dcf40f546f8618e825a93a7c564767d6e
yoiner <albreyacosta14@gmail.com>
2026-03-23 15:33:08 -0500

    concecxion de base de datos exitosa

--- php/db.php
+++ php/db.php
@@ -1,3 +1,17 @@
 <?php
-echo "hola mundo";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "supermercado";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+echo "conexion exitosa";
+$conn->set_charset("utf8mb4");
+?>
 ?>
