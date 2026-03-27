6e3c67b34c67e06a4674baf23b233f47d7bc3388
Cris <itscristopherr@gmail.com>
2026-03-27 18:58:37 -0400

    ingreso cambios en la bd

--- php/db.php
+++ php/db.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "supermercado";
+$servername = "sql312.infinityfree.com";
+$username = "if0_41495163";
+$password = "Evangelion1818";
+$dbname = "if0_41495163_mercablue";
 
 try {
-    $pdo = new PDO ("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch(PDOException $e) {
